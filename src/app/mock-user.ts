06fd77b839b8187e53c4c227ecb4ab9cb164ed76
Keemchard <keemchardtamio@gmail.com>
2022-02-22 11:42:42 +0800

    Deleted mock user array

--- src/app/mock-user.ts
+++ src/app/mock-user.ts
@@ -1,20 +1,3 @@
 import { userInterface } from './user-interface';
 
-export const userArray: userInterface[] = [
-  {
-    name: 'hery',
-    email: 'he@gmail.com',
-  },
-  {
-    name: 'herfdfdy',
-    email: 'hedfsdfy',
-  },
-  {
-    name: 'hery',
-    email: 'hedfdfy',
-  },
-  {
-    name: 'hery',
-    email: 'hey',
-  },
-];
+export const userArray: userInterface[] = [];
