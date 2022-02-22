266e616cc41ef257415be80cd230d9664ca98a83
Keemchard <keemchardtamio@gmail.com>
2022-02-22 09:54:55 +0800

    Created organized register folder for sign and log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './register/login/login.component';
+import { SignupComponent } from './register/signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
