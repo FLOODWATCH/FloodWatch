612326a1cf2bea5dcf0a3db46133ec434bf248eb
Keemchard <keemchardtamio@gmail.com>
2022-02-23 13:28:14 +0800

    Added login functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'login', pathMatch: 'full' },
