bd42461ddc761205ae86fa7c5eb2bea307f96b7b
ProgrammerJaneki <revillazjannelz@gmail.com>
2022-03-03 18:26:59 +0800

    Fixed Modal

--- src/app/register/login/login.component.ts
+++ src/app/register/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { userInterface } from 'src/app/user-interface';
 import { UserCredService } from 'src/app/register-services/user-cred.service';
 import { Router } from '@angular/router';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +12,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  faTimes = faTimes;
+  faCheck = faCheck;
+  faExclamation = faExclamation;
+  errorAlert: string;
+  loginModalMessage: string;
   userVariable: userInterface[] = [];
   constructor(private userObj: UserCredService, private router: Router) { }
 
@@ -16,6 +24,52 @@ export class LoginComponent implements OnInit {
   password: string;
   ngOnInit(): void { }
 
+  // Green Modal
+  greenModal() {
+    const secModal: HTMLInputElement = document.querySelector("#login-green-icon")
+    const modalContent: HTMLInputElement = document.querySelector("#login-modal-content")
+    const modalText: HTMLInputElement = document.querySelector("#login-modal-ptext")
+    const modalIcon: HTMLInputElement = document.querySelector("#login-icon-modal")
+    this.loginModalMessage = 'Success'
+    this.errorAlert = 'Login Success'
+    modalContent.style.color = 'rgb(39, 165, 87)'
+    modalContent.style.borderColor = 'rgb(4, 107, 4)'
+    modalContent.style.backgroundColor = 'rgb(191, 238, 201)'
+    modalText.style.color = 'rgb(97, 177, 127)'
+    modalIcon.style.backgroundColor = 'rgb(39, 165, 87)'
+    modalContent.style.borderColor = 'green'
+    secModal.style.display = 'flex'
+  }
+  // Red Modal
+  redModal() {
+    const thisModal: HTMLInputElement = document.querySelector("#login-red-icon")
+    const modalContent: HTMLInputElement = document.querySelector("#login-modal-content")
+    const modalText: HTMLInputElement = document.querySelector("#login-modal-ptext")
+    const modalIcon: HTMLInputElement = document.querySelector("#login-icon-modal")
+
+    this.loginModalMessage = 'Error'
+    this.errorAlert = 'Please fill everything'
+    modalContent.style.color = '#ad3939'
+    modalContent.style.borderColor = 'red'
+    modalContent.style.backgroundColor = 'rgb(255, 217, 211)'
+    modalText.style.color = 'rgb(214, 111, 111)'
+    modalIcon.style.backgroundColor = 'rgb(221, 82, 48)'
+    modalContent.style.borderColor = 'red'
+    thisModal.style.display = 'flex'
+  }
+
+  // Check Email
+  checkEmail() {
+    const getEmail: HTMLInputElement = document.querySelector("#email")
+    if (getEmail.value == '') { getEmail.style.borderColor = 'red' }
+    else { getEmail.style.borderColor = 'white' }
+  }
+  // Check Password
+  checkPassword() {
+    const getPassword: HTMLInputElement = document.querySelector("#password")
+    if (getPassword.value == '') { getPassword.style.borderColor = 'red' }
+    else { getPassword.style.borderColor = 'white' }
+  }
   resetFields() {
     var fieldID = ['#email', '#password'];
     for (let x = 0; x <= fieldID.length; x++) {
@@ -28,6 +82,8 @@ export class LoginComponent implements OnInit {
   login() {
     const first: HTMLInputElement = document.querySelector("#email")
     const second: HTMLInputElement = document.querySelector("#password")
+    // const checkEmail: HTMLInputElement = document.querySelector("#email")
+    const openModal: HTMLDivElement = document.querySelector("#my-modal")
 
     this.userObj.getUserFromService().subscribe((u) => {
       const theUser = u.find((a: any) => {
@@ -36,12 +92,24 @@ export class LoginComponent implements OnInit {
       if (theUser) {
         first.style.borderBottomColor = 'white';
         second.style.borderBottomColor = 'white';
-        alert('Login Success');
-        this.router.navigate(['dashboard']);
+        this.greenModal()
+        openModal.style.display = 'flex'
+        // this.router.navigate(['dashboard']);
       } else {
-        first.style.borderBottomColor = 'red';
-        second.style.borderBottomColor = 'red';
-        alert('User Not Found');
+        if (first.value == '' || second.value == '') {
+          this.redModal()
+          this.checkEmail()
+          this.checkPassword()
+          // this.thisModal = 'Please fill everything'
+          openModal.style.display = 'flex'
+        } else {
+          // first.style.borderBottomColor = 'red';
+          // second.style.borderBottomColor = 'red';
+          this.redModal()
+          this.loginModalMessage = 'Error'
+          this.errorAlert = 'User not found'
+          openModal.style.display = 'flex';
+        }
       }
     });
   }
@@ -52,5 +120,12 @@ export class LoginComponent implements OnInit {
     let signupDiv: HTMLDivElement = document.querySelector('.signup-div');
     signupDiv.style.display = 'block';
     this.resetFields();
+  }
+  closeSuccessModal() {
+    this.router.navigate(['dashboard']);
+  }
+  closeButton() {
+    var closeBtn: HTMLDivElement = document.querySelector("#my-modal");
+    closeBtn.style.display = 'none';
   }
 }
--- src/app/register/signup/signup.component.ts
+++ src/app/register/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { userInterface } from 'src/app/user-interface';
 import { UserCredService } from 'src/app/register-services/user-cred.service';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { userArray } from 'src/app/mock-user';
 import { element } from 'protractor';
 
@@ -12,11 +13,14 @@ import { element } from 'protractor';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  displayName = true;
   faTimes = faTimes;
   faExclamation = faExclamation;
+  faCheck = faCheck;
   userVariable: userInterface[] = [];
   focus: boolean;
-  modalMessage: string;
+  signupModalMessage: string;
+  alert: string;
 
   name: string;
   email: string;
@@ -42,25 +46,103 @@ export class SignupComponent implements OnInit {
   }
   // Error Indicator
   changeColor(element, bool) {
-    if (bool) {
-      // bool is true if error is found
-      element.style.borderColor = 'red';
+    // bool is true if error is found
+    if (bool) { element.style.borderColor = 'red' }
+    else { element.style.borderColor = 'white' }
+  }
+  // Green Modal
+  greenModal() {
+    this.displayName = false;
+    const greenClose: HTMLInputElement = document.querySelector("#signup-green-icon")
+    const modalContent: HTMLInputElement = document.querySelector("#modal-content")
+    const modalText: HTMLInputElement = document.querySelector("#modal-ptext")
+    const modalIcon: HTMLInputElement = document.querySelector("#icon-modal")
+    this.signupModalMessage = 'Success'
+    this.alert = 'Account Created'
+    modalContent.style.color = 'rgb(39, 165, 87)'
+    modalContent.style.borderColor = 'rgb(4, 107, 4)'
+    modalContent.style.backgroundColor = 'rgb(191, 238, 201)'
+    modalText.style.color = 'rgb(97, 177, 127)'
+    modalIcon.style.backgroundColor = 'rgb(39, 165, 87)'
+    modalContent.style.borderColor = 'green'
+    greenClose.style.display = 'flex'
+  }
+  // Red Modal
+  redModal() {
+    this.displayName = true;
+    const redClose: HTMLInputElement = document.querySelector("#signup-red-icon")
+    const modalContent: HTMLInputElement = document.querySelector("#modal-content")
+    const modalText: HTMLInputElement = document.querySelector("#modal-ptext")
+    const modalIcon: HTMLInputElement = document.querySelector("#icon-modal")
+
+    this.signupModalMessage = 'Error'
+    this.alert = 'Please fill everything'
+    modalContent.style.color = '#ad3939'
+    modalContent.style.borderColor = 'red'
+    modalContent.style.backgroundColor = 'rgb(255, 217, 211)'
+    modalText.style.color = 'rgb(214, 111, 111)'
+    modalIcon.style.backgroundColor = 'rgb(221, 82, 48)'
+    modalContent.style.borderColor = 'red'
+    redClose.style.display = 'flex'
+  }
+  //--------- ALL KEYUP VALIDATORS -----------//
+  // Keyup validation for name
+  validateName() {
+    var checkName: HTMLInputElement = document.querySelector("#name")
+    if (checkName.value == '') { checkName.style.borderColor = 'red' }
+    else { checkName.style.borderColor = 'white' }
+  }
+  // Keyup validation for email
+  validateEmail() {
+    var email: HTMLInputElement = document.querySelector('#em');
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.value.match(pattern)) { email.style.borderColor = 'white' }
+    else { email.style.borderColor = 'red' }
+  }
+  // Keyup validation for mobile no
+  mobileValidation() {
+    var thisNumber: HTMLInputElement = document.querySelector("#mobile")
+    var num = document.getElementById("mobile")
+
+    if (thisNumber.value.match(/[a-z]/g) || thisNumber.value.match(/[A_Z]/g)) {
+      thisNumber.style.borderColor = 'red'
     } else {
-      element.style.borderColor = 'white';
+      // checks if it's all numbers with max length of 11
+      if (thisNumber.value.match(/\d/g).length === 11) {
+        thisNumber.style.borderColor = 'white'
+      } else { thisNumber.style.borderColor = 'red' }
     }
   }
+  // Keyup validation for address
+  validateAddress() {
+    var checkAddress: HTMLInputElement = document.querySelector("#address")
+    if (checkAddress.value == '') { checkAddress.style.borderColor = 'red' }
+    else { checkAddress.style.borderColor = 'white' }
+  }
+  // Keyup validation of password
+  validatePassword() {
+    var pass: HTMLInputElement = document.querySelector("#pw")
+
+    if (
+      (pass.value.match(/[a-z]/g) || pass.value.match(/[A_Z]/g)) &&
+      pass.value.match(/[0-9]/g)
+    ) {
+      pass.style.borderColor = 'white'
+    } else {
+      pass.style.borderColor = 'red'
+    }
+  }
+  //--------- ALL NON-KEYUP VALIDATORS -----------//
   // Validates the email address
   emailValidation() {
     var email: HTMLInputElement = document.querySelector('#em');
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (email.value.match(pattern)) {
-      return true;
-    } else {
-      return false;
-    }
+    if (email.value.match(pattern)) { return true }
+    else { return false }
   }
-  // Validates the phone no.
+  // Validates the mobile no.
   phoneValidation(num) {
     if (num.match(/[a-z]/g) || num.match(/[A_Z]/g)) {
       return false;
@@ -71,7 +153,7 @@ export class SignupComponent implements OnInit {
       }
     }
   }
-  // checks if password contains both characters & numbers
+  // Validates if password contains both characters & numbers
   passwordValidation(pass) {
     if (
       (pass.match(/[a-z]/g) || pass.match(/[A_Z]/g)) &&
@@ -82,26 +164,28 @@ export class SignupComponent implements OnInit {
       return false;
     }
   }
-  // Checks all textfields if valid
+  // Validates all textfields if valid
   checkFields(name, email, mobile, address, password) {
     // var checkError = true;
     var thisField = [name, email, mobile, address, password];
     var thisID = ['#name', '#em', '#mobile', '#address', '#pw'];
     for (let i = 0; i <= thisID.length; i++) {
       const first = document.querySelector(`${thisID[i]}`);
-      const closeBtn: HTMLDivElement = document.querySelector("#myModal");
+      const showModal: HTMLDivElement = document.querySelector("#myModal");
+      const redClose: HTMLInputElement = document.querySelector("#signup-red-icon");
       // changeColor is true if error is found
       if (!thisField[i]) {
         this.changeColor(first, true);
         // Shows Modal
-        this.modalMessage = 'Please fill everything'
-        closeBtn.style.display = 'flex';
+        // this.signupModalMessage = 'Error'
+        // this.alert = 'Please fill everything'
+        this.redModal()
+        showModal.style.display = 'flex';
+        redClose.style.display = 'flex';
       } else if (thisField[i] == email) {
         if (this.emailValidation()) {
           this.changeColor(first, false);
         } else {
-          // alert('incorrect')
-          // this.emailValidation();
           this.changeColor(first, true);
         }
       } else if (thisField[i] == mobile) {
@@ -145,7 +229,8 @@ export class SignupComponent implements OnInit {
         newUser.address,
         newUser.password
       );
-    } else if (!this.emailValidation()) {
+    }
+    else if (!this.emailValidation) {
       this.checkFields(
         newUser.name,
         newUser.email,
@@ -169,21 +254,15 @@ export class SignupComponent implements OnInit {
         newUser.address,
         newUser.password
       );
-    } else {
-      // var delayMessage = 1000;
-      setTimeout(this.toggleLoginForm, 500);
-      // setTimeout(function () {
-      //   this.toggleLoginForm()
-      // }, delayMessage)
-      this.modalMessage = 'Successful!'
-      openModal.style.display = 'flex';
+    }
+    else {
       this.userObj
         .addUserFromService(newUser)
         .subscribe((u) => this.userVariable.push(newUser));
-      // this.toggleLoginForm()
+      this.greenModal()
+      openModal.style.display = 'flex'
     }
   }
-
   toggleLoginForm() {
     let loginDiv: HTMLDivElement = document.querySelector('.login-div');
     loginDiv.style.display = 'block';
@@ -191,9 +270,23 @@ export class SignupComponent implements OnInit {
     signupDiv.style.display = 'none';
     this.resetFields();
   }
-
+  // Close button when account is created
+  toggleClose() {
+    var showModal: HTMLDivElement = document.querySelector("#myModal");
+    showModal.style.display = 'none'
+    this.toggleLoginForm()
+  }
+  // Close button when error is found
   closeButton() {
-    var closeBtn: HTMLDivElement = document.querySelector("#myModal");
-    closeBtn.style.display = 'none';
+    var showModal: HTMLDivElement = document.querySelector("#myModal");
+    showModal.style.display = 'none';
+  }
+  checkKey() {
+    var myName: HTMLInputElement = document.querySelector("#name");
+    if (myName.value.length < 11 && myName.value != '') {
+      myName.style.borderColor = "red";
+    } else {
+      myName.style.borderColor = "white";
+    }
   }
 }
