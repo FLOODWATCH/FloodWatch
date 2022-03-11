259f7e5d8923dea25ff5d95a652c26a785746297
Keemchard <keemchardtamio@gmail.com>
2022-03-11 15:25:41 +0800

    linked post backend but commented

--- src/app/dashboard-service/post.service.ts
+++ src/app/dashboard-service/post.service.ts
@@ -12,10 +12,12 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class PostService {
-  private postUrl = 'http://localhost:5000/post';
+  private postUrl = 'http://localhost:5000/post'; //from json-server
+  private flpostUrl = 'http://localhost:8080/flpost'; //from spring boot
+
   constructor(private http: HttpClient) {}
 
-  //getPost
+  getPost;
   getPostFromPostService(): Observable<postInterface[]> {
     return this.http.get<postInterface[]>(this.postUrl);
   }
@@ -30,4 +32,18 @@ export class PostService {
     const deleteUrl = `${this.postUrl}/${postDeleted.id}`;
     return this.http.delete<postInterface>(deleteUrl);
   }
+
+  //Using Spring boot as backend - START (WILL UNCOMMENT THIS ONCE WE'VE ELIMINATE JSON SERVER FROM THE SYSTEM HAHAHA)
+  // getPostFromPostService(): Observable<postInterface[]> {
+  //   return this.http.get<postInterface[]>(this.flpostUrl);
+  // }
+
+  // addPostFromPostService(postAdded: postInterface): Observable<postInterface> {
+  //   return this.http.post<postInterface>(
+  //     this.flpostUrl,
+  //     postAdded,
+  //     httpOptions
+  //   );
+  // }
+  //Using Spring boot as backend - END
 }
