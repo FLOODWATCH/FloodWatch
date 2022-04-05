6f36ad51e35a84148fcebc9040fad6435e9298bb
Keemchard <keemchardtamio@gmail.com>
2022-04-05 16:53:11 +0800

    FLUpdates Done

--- src/app/dashboard/flupdates/flupdates.component.ts
+++ src/app/dashboard/flupdates/flupdates.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { waterLevelPostInterface } from 'src/app/WaterLevelPost-Interface';
+import { FlupdatesService } from 'src/app/dashboard-service/flupdates.service';
 
 @Component({
   selector: 'app-flupdates',
   templateUrl: './flupdates.component.html',
-  styleUrls: ['./flupdates.component.css']
+  styleUrls: ['./flupdates.component.css'],
 })
 export class FlupdatesComponent implements OnInit {
-
-  constructor() { }
+  flUpdatesVariable: waterLevelPostInterface[] = [];
+  constructor(private flUpdateServiceObj: FlupdatesService) {}
 
   ngOnInit(): void {
+    this.flUpdateServiceObj
+      .getFLUpdatesFromService()
+      .subscribe((flupdates) => (this.flUpdatesVariable = flupdates));
   }
-
 }
