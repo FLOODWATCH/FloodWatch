d23c398f17ef5a156d92a693783aa88e5a2c1d5a
Keemchard <keemchardtamio@gmail.com>
2022-05-02 20:39:22 +0800

    added notifications in display

--- src/app/dashboard/notif/notif.component.ts
+++ src/app/dashboard/notif/notif.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { waterLevelPostInterface } from 'src/app/WaterLevelPost-Interface';
+import { FlupdatesService } from 'src/app/dashboard-service/flupdates.service';
 
 @Component({
   selector: 'app-notif',
   templateUrl: './notif.component.html',
-  styleUrls: ['./notif.component.css']
+  styleUrls: ['./notif.component.css'],
 })
 export class NotifComponent implements OnInit {
+  notifVar: waterLevelPostInterface[] = [];
 
-  constructor() { }
+  constructor(private flUpdateServiceObj: FlupdatesService) {}
 
   ngOnInit(): void {
+    this.flUpdateServiceObj
+      .getFLUpdatesFromService()
+      .subscribe((notif) => (this.notifVar = notif));
   }
-
 }
