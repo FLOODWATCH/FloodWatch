811c62d55ccecd586c86a2b5c556809931558811
ProgrammerJaneki <revillazjannelz@gmail.com>
2022-05-19 18:28:45 +0800

    Added File Upload and Delete

--- src/app/dashboard/post/post.component.ts
+++ src/app/dashboard/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { postInterface } from 'src/app/mock-post-interface';
+import { imageInterface } from 'src/app/mock-image-interface';
 import { postArray } from 'src/app/mock-post-array';
 import { PostService } from 'src/app/dashboard-service/post.service';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -12,13 +13,15 @@ import { faFileImage } from '@fortawesome/free-regular-svg-icons';
 import { faFileVideo } from '@fortawesome/free-regular-svg-icons';
 import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
 import { profile, timeStamp } from 'console';
-import { stringify } from 'querystring';
+import { decode, stringify } from 'querystring';
 import { HttpClient, HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
-import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators, NgForm } from '@angular/forms';
 import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { FileService } from 'src/app/dashboard-service/file.service';
 import { url } from 'inspector';
 import { isNull } from '@angular/compiler/src/output/output_ast';
+import { type } from 'os';
+import { deepStrictEqual } from 'assert';
 
 @Component({
   selector: 'app-post',
@@ -28,14 +31,22 @@ import { isNull } from '@angular/compiler/src/output/output_ast';
 export class PostComponent implements OnInit {
   // for file
   images = [];
-  url: any;
+  thisImages: any;
   base64Data: any;
   retrieveResponse: any;
   selectedFile: File;
   retrievedImage: any;
-  private base64textString: string;
+  thisArray = ["Hello", "Hi", "YO"];
+  imageSrc = 'assets/yaeWP.jpg'
+  // imageArray = ['assets/yaeWP.jpg', 'assets/bg.jpg', 'assets/register-bg.jpg']
+  imageArray = [];
 
   cnt: number;
+  private count = 0;
+  nameContainer = [];
+  folderName = "first";
+  imagename: string;
+  sampleImage: any;
 
   profIdNumber: string;
   profName: string;
@@ -56,27 +67,21 @@ export class PostComponent implements OnInit {
   updatePostTime: string;
   holder;
   postVariable: postInterface[] = [];
+  imageTracker: imageInterface[] = [];
   constructor(private postObj: PostService, private httpClient: HttpClient,
     private fileService: FileService) { }
 
   ngOnInit(): void {
-    // this.postObj
-    //   .getPostFromPostService()
-    //   .subscribe(
-    //     p => {
-    //       this.postVariable = p;
-    //       console.log("P:")
-    //       console.log(p[23])
-    //       this.retrieveResponse = p[23];
-    //       this.base64Data = this.retrieveResponse.profFile;
-    //       this.retrievedImage = this.base64Data;
-    //       this.profFile = this.retrievedImage;
-    //       console.log(this.profFile)
-    //     }
-    //   )
     this.postObj
       .getPostFromPostService()
-      .subscribe((p) => (this.postVariable = p));
+      .subscribe((p) => {
+        this.postVariable = p;
+      });
+    this.postObj
+      .getImagesFromService()
+      .subscribe((i) => {
+        this.imageTracker = i;
+      })
 
     let flupdatesConVar: HTMLDivElement =
       document.querySelector('.flupdates-con');
@@ -87,6 +92,8 @@ export class PostComponent implements OnInit {
     diagramConVar.style.display = 'none';
     this.closeUpdateForm();
   }
+
+  // After ngOnInit
   thisUrl = "./assets/yaeWP.jpg";
 
   closeTimes() {
@@ -109,7 +116,6 @@ export class PostComponent implements OnInit {
     text.style.height = '200px'
     imageContainer.style.display = 'none'
     this.images.splice(0, this.images.length)
-    console.log(this.images.length)
     // if (postContent.value != null ) {
     //   this.errorModal("Notice", "Discard Post?")
     // }
@@ -177,6 +183,8 @@ export class PostComponent implements OnInit {
     const postName: HTMLHeadingElement = document.querySelector('#profileName');
     const postEmail: HTMLHeadingElement =
       document.querySelector('#profileEmail');
+    this.thisArray.push(this.profTextPost);
+    // FormData
     const newPost = {
       profName: (this.profName = postName.textContent),
       profEmail: (this.profEmail = postEmail.textContent),
@@ -184,27 +192,37 @@ export class PostComponent implements OnInit {
       profPostTime: (this.profPostTime = String(
         date.toLocaleTimeString('en-US', format)
       )),
-      profFile: (this.images.length == 0 ? null : this.images)
-      // profFile: (this.profFile = formData.append('file',
-      //   this.myForm.get('fileSource').value))
+      // profFile: (!this.imagename ? null : this.imagename)
+      profFile: (this.images.length == 0 ? null : this.imagename)
+
     };
-    console.log("Profile")
-    console.group(this.selectedFile)
-    // let postContent: HTMLTextAreaElement =
-    //   document.querySelector('.post-content');
     if (!this.profTextPost) {
       this.errorModal('Error', 'Post cannot be void');
       // return alert('Post cannot be void');
     } else {
+      // Post Upload
       this.postObj
         .addPostFromPostService(newPost)
         .subscribe((p) => this.postVariable.push(newPost));
       this.profTextPost = null;
-      // Just change the value into 2 for json server. Although 4 get requests will still work for json
+      if (this.images.length != 0) {
+        // Image Upload
+        const uploadImageData = new FormData();
+        uploadImageData.append('file', this.selectedFile, this.selectedFile.name)
+        this.postObj.uploadImageFromService(uploadImageData)
+          .subscribe((i) => (console.log(i)));
+      }
       for (let x = 0; x < 4; x++) {
         this.postObj
           .getPostFromPostService()
           .subscribe((p) => (this.postVariable = p));
+      }
+      for (let x = 0; x < 4; x++) {
+        this.postObj
+          .getImagesFromService()
+          .subscribe((i) => {
+            this.imageTracker = i;
+          })
       }
       this.successModdal('Success', 'Post Uploaded');
       this.closeTimes();
@@ -217,10 +235,6 @@ export class PostComponent implements OnInit {
       document.querySelector('#profileName');
     const profileEmail: HTMLParagraphElement =
       document.querySelector('#profileEmail');
-    // In order to refresh
-    // this.postObj
-    //   .getPostFromPostService()
-    //   .subscribe((p) => (this.postVariable = p));
     if (
       profileName.textContent === postToBeDeleted.profName &&
       profileEmail.textContent === postToBeDeleted.profEmail
@@ -233,33 +247,35 @@ export class PostComponent implements OnInit {
             (p) => p.id !== postToBeDeleted.id
           ))
         );
+      for (let img of this.imageTracker) {
+        if (postToBeDeleted.profFile === img.filename) {
+          console.log(img)
+          this.postObj.deleteImageFromService(img)
+            .subscribe(() => (this.postVariable = this.postVariable.filter(
+              (d) => d.id !== img.id
+            )))
+        }
+      }
       this.successModdal('Success', 'Post Deleted');
-      // this.postObj
-      //   .getPostFromPostService()
-      //   .subscribe((p) => (this.postVariable = p));
     } else {
       this.errorModal('Error', 'Not Your Account!');
-      // alert('This is not your post, bakit mo ide delete aber???');
     }
   }
 
   //toggle update form
   toggleUpdatePostForm(postTobeUpdated: postInterface) {
-    // this.postObj
-    //   .getPostFromPostService()
-    //   .subscribe((p) => (this.postVariable = p));
     const profileName: HTMLHeadingElement =
       document.querySelector('#profileName');
     const profileEmail: HTMLParagraphElement =
       document.querySelector('#profileEmail');
-    let optionTab: HTMLDivElement = document.querySelector('.post-option-tab');
-    optionTab.style.display = 'none';
+    const updateImg: HTMLInputElement = document.querySelector('#post-update-files')
+    const adjustJustify: HTMLDivElement = document.querySelector('.update-post-container')
+    const updateImage: HTMLDivElement = document.querySelector("#img-update-container")
+    const text: HTMLTextAreaElement = document.querySelector("#updatePostText")
     if (
       profileName.textContent === postTobeUpdated.profName &&
       profileEmail.textContent === postTobeUpdated.profEmail
     ) {
-      // passes the post to be updated into the holder that
-      // we can access outside the function
       this.holder = postTobeUpdated;
       // Opens the update form
       let mainUpdateCon: HTMLDivElement =
@@ -268,34 +284,43 @@ export class PostComponent implements OnInit {
       let updateTextArea: HTMLTextAreaElement =
         document.querySelector('.update-textarea');
       updateTextArea.value = postTobeUpdated.profTextPost;
+      // If it contains image
+      if (postTobeUpdated.profFile) {
+        updateImage.style.display = 'flex'
+        text.style.height = '120px'
+        for (let img of this.imageTracker) {
+          if (postTobeUpdated.profFile === img.filename) {
+            console.log(typeof img.filename)
+            const imageUpdate = document.getElementById("img-update-file") as HTMLImageElement;
+            imageUpdate.src = 'data:image/jpeg;base64,' + img.data
+          }
+        }
+        updateImg.style.display = 'none'
+        adjustJustify.style.justifyContent = 'flex-end'
+      } else {
+        text.style.height = ''
+        adjustJustify.style.justifyContent = 'flex-end'
+      }
+
     } else {
       this.errorModal('Error', 'Not Your Account!');
-      // alert('This is not your post, bakit mo i a update, aber???');
     }
   }
 
   // Update the post, toggleUpdatePostForm
   updateThePost() {
-    // this.postObj
-    //   .getPostFromPostService()
-    //   .subscribe((p) => (this.postVariable = p));
     let postContent: HTMLTextAreaElement =
       document.querySelector('#updatePostText');
-    // alert(`${this.holder.id}`)
     this.holder.profTextPost = postContent.value;
     this.postObj.updatePostFromPostService(this.holder).subscribe();
-    // this.postObj
-    //   .getPostFromPostService()
-    //   .subscribe((p) => (this.postVariable = p));
-    // this.closeUpdateForm()
     this.successModdal('Success', 'Post Updated');
     this.closeUpdateForm();
   }
 
   closeUpdateForm() {
-    // this.postObj
-    //   .getPostFromPostService()
-    //   .subscribe((p) => (this.postVariable = p));
+    const updateImage: HTMLDivElement = document.querySelector("#img-update-container")
+    const text: HTMLTextAreaElement = document.querySelector("#updatePostText")
+    updateImage.style.display = 'none'
     let mainUpdateCon: HTMLDivElement =
       document.querySelector('.main-update-con');
     mainUpdateCon.style.display = 'none';
@@ -418,12 +443,6 @@ export class PostComponent implements OnInit {
       document.querySelector('#dashboard-modal');
     closeModal.style.display = 'flex';
     this.successModdal('Success', 'Post Uploaded');
-    // this.errorModal("Error", "Not Your Account")
-    // let messageType = document.getElementById("dashboard-message-type");
-    // messageType.innerHTML = "Success"
-    // let modalMessage = document.getElementById("dashboard-modal-message");
-    // modalMessage.innerHTML = "Post Uploaded"
-    // dashboard-modal-message
   }
 
   // Modal Message
@@ -457,7 +476,6 @@ export class PostComponent implements OnInit {
     messageModal.style.color = 'rgb(97, 177, 127)';
     alertType.style.backgroundColor = 'rgb(39, 165, 87)';
     typeMessage.style.color = 'rgb(39, 165, 87)';
-    // messageModal.style.color = 'rgb(214, 111, 111)'
   }
   errorModal(messageType, modalMessage) {
     const closeModal: HTMLDivElement =
@@ -492,18 +510,13 @@ export class PostComponent implements OnInit {
   }
   uploadImg() {
     const text: HTMLTextAreaElement = document.querySelector("#post-content")
-    const postForm: HTMLFormElement = document.querySelector("#post-create-form")
     const imageContainer: HTMLDivElement = document.querySelector("#img-container")
-    const imgClose = document.getElementById("img-close")
     text.style.height = '120px'
-    // const fileContainer: HTMLDivElement = document.querySelector("#img-file-container")
-    // fileContainer.style.display = 'flex'
-
     imageContainer.style.display = 'flex'
-    // postForm.style.height = '700px'
   }
 
   deleteImage(i) {
+
     console.log(i)
     this.images.splice(i, 1);
     this.images = this.images.filter((a) => a !== i);
@@ -512,41 +525,38 @@ export class PostComponent implements OnInit {
   }
   // Upload Image
   selectFile(event) {
-    const imageContainer: HTMLDivElement = document.querySelector("#img-container")
-    // if (event.target.files && event.target.files[0]) {
-    if (event.target.files.length <= 4 && this.images.length < 4) {
+    this.selectedFile = event.target.files[0];
+    this.count = event.target.files.length;
+    if (event.target.files.length <= 1 && this.images.length < 1) {
+      var changeName = 0;
       var filesAmount = event.target.files.length;
+      const formData = new FormData()
       for (let i = 0; i < filesAmount; i++) {
         var reader = new FileReader()
-        console.log('No: ', filesAmount)
         reader.onload = (event) => {
           this.images.push(event.target.result);
         }
-        reader.onload = this.handleReader.bind(this);
-        reader.readAsBinaryString(event.target.files[i])
-        // reader.readAsDataURL(event.target.files[i]);
+        reader.readAsDataURL(event.target.files[i])
+        var fileName = event.target.files[i].name;
+        console.log(fileName);
+        this.imagename = fileName;
+        // reader.onload = this.handleReader.bind(this);
+        // reader.readAsBinaryString(event.target.files[i])
       }
-      console.log('Images: ', this.images.length)
     } else {
       this.errorModal('Error', 'Max of 4 images only')
     }
-    // }
   }
   handleReader(e) {
     this.images.push('data:image/png;base64,' + btoa(e.target.result));
-    for (let i = 0; i < this.images.length; i++) {
-      // console.log("NO: " + i);
-      // console.log(this.images[i]);
-    }
+    console.log(typeof this.thisArray)
+    // var decoder = this.toUTF8Array(this.images[0]);
+    // console.log(btoa(e.target.result))
+    this.thisImages = ('data:image/png;base64,' + btoa(e.target.result));
+    console.log(this.thisImages)
+    console.log(this.images.toString())
   }
 
-  // _handleReaderLoaded(readerEvt) {
-  //   var binaryString = readerEvt.target.result;
-  //   this.base64textString = btoa(binaryString);
-  //   console.log("Handle Reader: ")
-  //   this.profFile = this.base64textString
-  //   console.log(this.profFile)
-  // }
   uploadImage() {
     console.log(this.selectedFile);
     console.log(this.selectedFile.name)
@@ -582,27 +592,5 @@ export class PostComponent implements OnInit {
 
         }
       );
-    // this.thisUrl = this.retrievedImage;
   }
-  increaseSize() {
-    const elem = document.getElementById("img-file")
-    const close: HTMLImageElement = document.querySelector('#close-img');
-    const con: HTMLDivElement = document.querySelector('#each-container')
-    const nod = document.getElementById('img-file');
-    // nod.parentElement.style.opacity = '0.5'
-    this.cnt = 1;
-    console.log("increase", this.cnt)
-  }
-  decreaseSize() {
-    const elem = document.getElementById("img-file")
-    const close: HTMLImageElement = document.querySelector('#close-img');
-    const nod = document.getElementById('img-file');
-    // nod.parentElement.style.opacity = '1'
-    this.cnt = 0;
-    console.log("decrease", this.cnt)
-    // close.style.opacity = '0';
-    // close.style.display = 'block'
-    // elem.style.opacity = '1';
-  }
-
 }
